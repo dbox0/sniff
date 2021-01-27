ed617b2764de622ac60c4e31826a399d65db357f
dbox0 <georgi.dimov96@gmail.com>
2021-01-27 17:27:41 +0100

    no major progess

--- scrape.py
+++ scrape.py
@@ -1,19 +1,36 @@
 from urllib.request import urlopen
+import re
 
-url = "http://funnyjunk.com/"
+url = "http://www.webmfiles.org/demo-files/"
 
 page = urlopen(url)
 html_bytes = page.read()
-
-
 html = html_bytes.decode("utf-8")
-title_index = html.find("<title>")
-title_start = title_index + len("<title>")
-title_end = html.find("</title>")
 
-title = html[title_start:title_end]
-print(title)
+def getTitle():
+	title_index = html.find("<title>")
+	title_start = title_index + len("<title>")
+	title_end = html.find("</title>")
+	title = html[title_start:title_end]
+	print(title)
 
+
+
+def getBody():
+	body_index = html.find("<body>")
+	body_start = body_index + len("<body.")
+	body_end = html.find("</body>")
+	body = html[body_start:body_end]
+	return body
+
+def printbody():
+	body = getBody()
+	print(body)
+
+def findwebm():
+	body = getBody()
+	webms  = re.findall(".webm",body)
+	print(webms)
 
 #print(html)
 
